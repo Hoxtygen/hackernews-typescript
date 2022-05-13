c1748f462d433735daf2e61a02b90111bab7a847
Wasiu Idowu <hoxtygen@live.com>
2022-05-13 16:54:44 +0200

    Initial project setup

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { ApolloServer } from "apollo-server";
+import { schema } from './schema';
+
+export const server = new ApolloServer({
+	schema
+})
+
+const port = 3001;
+server.listen({ port }).then(({ url }) => {
+	console.log(`Server ready at ${url}`)
+})
