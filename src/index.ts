04b9239d5d485cf2730ce50d0a8c968fb6d5801f
Wasiu Idowu <hoxtygen@live.com>
2022-05-15 14:36:34 +0100

    Updated index.ts and created new scripts in package.json

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import { ApolloServer } from "apollo-server";
+import {ApolloServerPluginLandingPageLocalDefault} from "apollo-server-core"
 import { context } from "./context";
 import { schema } from './schema';
 
 export const server = new ApolloServer({
 	schema,
-	context
+	context,
+	introspection: true,
+	plugins:[ApolloServerPluginLandingPageLocalDefault()]
 })
 
-const port = 3001;
+const port = process.env.PORT||3001;
 server.listen({ port }).then(({ url }) => {
 	console.log(`Server ready at ${url}`)
 })
